6a5ea93c875e8c5811b22bded45f80162a5b043c
LucasLoustalot <lucas.loustalot@epitech.eu>
2026-05-01 16:11:00 -0400

    FIX: build for fedora 42 latest

--- frontend/cmake/os-linux.cmake
+++ frontend/cmake/os-linux.cmake
@@ -3,7 +3,8 @@ target_compile_definitions(
   obs-studio
   PRIVATE OBS_INSTALL_PREFIX="${OBS_INSTALL_PREFIX}" $<$<BOOL:${ENABLE_PORTABLE_CONFIG}>:ENABLE_PORTABLE_CONFIG>
 )
-target_link_libraries(obs-studio PRIVATE Qt::GuiPrivate Qt::DBus)
+find_package(Qt6 REQUIRED COMPONENTS GuiPrivate)
+target_link_libraries(obs-studio PRIVATE Qt6::GuiPrivate Qt::DBus)
 
 find_package(Libpci REQUIRED)
 target_link_libraries(obs-studio PRIVATE Libpci::pci)
