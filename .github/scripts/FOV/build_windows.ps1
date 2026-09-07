3c02c54566b0b6fcd29bc47b4e3d69e056f9a2eb
LucasLoustalot <lucas.loustalot@epitech.eu>
2026-09-07 17:23:40 +0200

    UPDATE: build scripts with release pressets + removed old build methods

--- .github/scripts/FOV/build_windows.ps1
+++ .github/scripts/FOV/build_windows.ps1
@@ -1,5 +1,5 @@
 git submodule update --init --recursive
-cmake --preset=windows-x64
+cmake --preset=windows-x64 -DENABLE_PORTABLE_CONFIG=ON
 cd build_x64
 cmake --build .
 cd ..
---
+++ build_portable_linux.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+read -p "Clean build ? [y/N]: " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+rm -rf build
+fi
+
+
+cmake --preset=ubuntu -DCMAKE_BUILD_TYPE=Debug -DENABLE_WEBRTC=On -DLINUX_PORTABLE=ON -DENABLE_RELOCATABLE=ON -DENABLE_PORTABLE_CONFIG=ON .
+
+cd build
+
+cmake --build .
+
+read -p "Run OBS now ? [y/N]: " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+./rundir/RelWithDebInfo/bin/obs --portable
+else
+exit 0
+fi
+
+#sudo cmake --install .
